f2078c7f4d044f3327fa1b8a2c8eafd240f6382a
Marcos Willian <willian.marcos7@hotmail.com>
2023-12-06 17:03:33 -0300

    improvement in board printing

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -2,7 +2,12 @@ package chess;
 
 import boardgame.Board;
 import boardgame.Position;
+import chess.pieces.Bishop;
+import chess.pieces.King;
+import chess.pieces.Knight;
 import chess.pieces.Pawn;
+import chess.pieces.Queen;
+import chess.pieces.Rook;
 
 public class ChessMatch {
     private int turn;
@@ -50,5 +55,33 @@ public class ChessMatch {
             placeNewPiece(i, 7, new Pawn(board, Color.BLACK));
             placeNewPiece(i, 2, new Pawn(board, Color.WHITE));
         }
+
+        //Place kings
+        placeNewPiece('e', 1, new King(board, Color.WHITE));
+        placeNewPiece('e', 8, new King(board, Color.BLACK));
+
+        //Place queens
+        placeNewPiece('d', 1, new Queen(board, Color.WHITE));
+        placeNewPiece('d', 8, new Queen(board, Color.BLACK));
+
+        //Place rooks
+        placeNewPiece('a', 1, new Rook(board, Color.WHITE));
+        placeNewPiece('a', 8, new Rook(board, Color.BLACK));
+        placeNewPiece('h', 1, new Rook(board, Color.WHITE));
+        placeNewPiece('h', 8, new Rook(board, Color.BLACK));
+
+        //Place knights
+        placeNewPiece('b', 1, new Knight(board, Color.WHITE));
+        placeNewPiece('b', 8, new Knight(board, Color.BLACK));
+        placeNewPiece('g', 1, new Knight(board, Color.WHITE));
+        placeNewPiece('g', 8, new Knight(board, Color.BLACK));
+
+        //Place bishops
+        placeNewPiece('c', 1, new Bishop(board, Color.WHITE));
+        placeNewPiece('c', 8, new Bishop(board, Color.BLACK));
+        placeNewPiece('f', 1, new Bishop(board, Color.WHITE));
+        placeNewPiece('f', 8, new Bishop(board, Color.BLACK));
+
+        
     }
 }
